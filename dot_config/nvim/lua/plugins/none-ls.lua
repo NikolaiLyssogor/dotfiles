f850c20a44c41d1ebd30faac6900bba838309c08
NikolaiLyssogor <lyssogor@gmail.com>
2024-11-19 22:22:08 -0800

    nvim: remove ruff from none-ls

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -9,7 +9,6 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.shfmt,
-				null_ls.builtins.formatting.ruff,
 				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.prettier,
 			},
