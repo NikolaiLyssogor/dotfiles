1e6772e4df861ac274b3e35691367013fcc4d886
NikolaiLyssogor <lyssogor@gmail.com>
2025-01-17 17:45:35 -0800

    nvim: add feynman quote to startup screen

--- dot_config/nvim/lua/plugins/startup-screen.lua
+++ dot_config/nvim/lua/plugins/startup-screen.lua
@@ -280,6 +280,11 @@ return {
 						"",
 						"—Richard P. Feynman, Surely You're Joking, Mr. Feynman!: Adventures of a Curious Character ",
 					},
+					{
+						"You have to keep a dozen of your favorite problems constantly present in your mind, although by and large they will lay in a dormant state. Every time you hear or read a new trick or a new result, test it against each of your twelve problems to see whether it helps. Every once in a while there will be a hit, and people will say, 'How did he do it? He must be a genius!",
+						"",
+						"—Richard P. Feynman",
+					},
 					{ "How you look at it is pretty much how you'll see it", "", "— Rasheed Ogunlaru" },
 					{
 						"That's the thing about people who think they hate computers. What they really hate is lousy programmers.",
