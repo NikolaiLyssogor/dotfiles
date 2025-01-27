5fbb5f08c31d99b9d9133df5e3cf03ac47f8d04d
NikolaiLyssogor <lyssogor@gmail.com>
2025-01-27 00:21:26 -0700

    nvim: updates to sink.nvim

--- dot_config/nvim/lua/plugins/sink.lua
+++ dot_config/nvim/lua/plugins/sink.lua
@@ -2,14 +2,15 @@ return {
 	{
     "NikolaiLyssogor/sink.nvim",
 		keys = {
-			{ "<leader>Rp", "<cmd>SinkPush<CR>", desc = "[s]ync to remote" },
+			{ "<leader>Rp", "<cmd>SinkPush<CR>", desc = "[p]ush to remote" },
+			{ "<leader>Rl", "<cmd>SinkPull<CR>", desc = "pu[l]l from remote" },
 		},
 		config = function()
 			require("sink").setup({
 				{
 					source = "/Users/nlyssogor/Documents/research/sync/",
 					dest = "nily9199@login.rc.colorado.edu:/projects/nily9199/sync/",
-					exclude = "/Users/nlyssogor/Documents/research/scripts/rsync-exclude.txt",
+					exclude = "/Users/nlyssogor/Documents/research/sync/rsync-exclude.txt",
 				},
 			})
 		end,
