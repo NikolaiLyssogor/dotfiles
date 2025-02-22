bc99c09ceb26879afa93c3e755aada0b3ab219d0
NikolaiLyssogor <lyssogor@gmail.com>
2025-02-22 12:23:41 -0800

    nvim: completions improvements

--- dot_config/nvim/lua/plugins/completions.lua
+++ dot_config/nvim/lua/plugins/completions.lua
@@ -1,40 +1,35 @@
 return {
-	{
-		"saghen/blink.cmp",
-		-- use a release tag to download pre-built binaries
-		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		-- build = 'cargo build --release',
-		-- If you use nix, you can build from source using latest nightly rust with:
-		-- build = 'nix run .#build-plugin',
-		version = "*",
-		pin = true,
+  {
+    "saghen/blink.cmp",
+    tag = "v0.11.0",
+    -- pin = true,
+    opts = {
 
-		opts = {
+      keymap = {
+        preset = "default",
+        ["<Tab>"] = { "accept", "fallback" },
+      },
 
-			keymap = {
-				preset = "default",
-				["<Tab>"] = { "accept", "fallback" },
-			},
+      appearance = {
+        use_nvim_cmp_as_default = false,
+        nerd_font_variant = "mono",
+      },
 
-			appearance = {
-				use_nvim_cmp_as_default = false,
-				nerd_font_variant = "mono",
-			},
+      snippets = { preset = "default" },
 
-			snippets = { preset = "default" },
+      sources = {
+        default = { "lsp", "path", "snippets" },
+      },
 
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
-			},
+      completion = {
+        accept = { auto_brackets = { enabled = true } },
+        documentation = { window = { border = "single" } },
+        menu = { border = "single" },
+      },
 
-			completion = {
-				menu = { border = "single" },
-				documentation = { window = { border = "single" } },
-			},
+      signature = { enabled = true, window = { border = "single" } },
+    },
 
-			signature = { enabled = true, window = { border = "single" } },
-		},
-
-		opts_extend = { "sources.default" },
-	},
+    opts_extend = { "sources.default" },
+  },
 }
