29e12acb456090fd88b308c8c069f7aa4f4ab7bd
NikolaiLyssogor <lyssogor@gmail.com>
2025-03-09 15:43:50 -0700

    nvim: update sink.nvim config

--- dot_config/nvim/lua/plugins/sink.lua
+++ dot_config/nvim/lua/plugins/sink.lua
@@ -1,37 +1,49 @@
 return {
-	{
-		"NikolaiLyssogor/sink.nvim",
-		-- dir = "~/Documents/personal-projects/sink.nvim/",
-		-- dev = true,
-		keys = {
-			{ "<leader>rp", "<cmd>SinkPush<CR>", desc = "[p]ush to remote" },
-			{ "<leader>rl", "<cmd>SinkPull<CR>", desc = "pu[l]l from remote" },
-		},
-		config = function()
-			require("sink").setup({
-				{
-					directory = "/Users/nlyssogor/Documents/research/sync/HIA/",
-					push = {
-						args = {
-							"-avz",
-							"--delete",
-							"--exclude-from",
-							"/Users/nlyssogor/Documents/research/sync/rsync-exclude.txt",
-							"/Users/nlyssogor/Documents/research/sync/HIA/src/",
-							"nily9199@login.rc.colorado.edu:/projects/nily9199/sync/HIA/src/",
-						},
-					},
-					pull = {
-						args = {
-							"-avz",
-							"--exclude-from",
-							"/Users/nlyssogor/Documents/research/sync/rsync-exclude.txt",
-							"nily9199@login.rc.colorado.edu:/projects/nily9199/sync/HIA/notebooks/",
-							"/Users/nlyssogor/Documents/research/sync/HIA/notebooks/",
-						},
-					},
-				},
-			})
-		end,
-	},
+  {
+    dir = "/Users/nlyssogor/Documents/personal-projects/sink.nvim",
+    dev = true,
+    keys = {
+      { "<leader>rp", "<cmd>SinkPush<CR>", desc = "[p]ush to remote" },
+      { "<leader>rl", "<cmd>SinkPull<CR>", desc = "pu[l]l from remote" },
+    },
+    config = function()
+      require("sink").setup({
+        paths = {
+          ["/Users/nlyssogor/Documents/research/sync/HIA/"] = {
+            push = {
+              args = {
+                "-avz",
+                "--delete",
+                "--exclude-from",
+                "/Users/nlyssogor/Documents/research/sync/rsync-exclude.txt",
+                "/Users/nlyssogor/Documents/research/sync/HIA/src/",
+                "nily9199@login.rc.colorado.edu:/projects/nily9199/sync/HIA/src/",
+              },
+            },
+            pull = {
+              args = {
+                "-avz",
+                "--exclude-from",
+                "/Users/nlyssogor/Documents/research/sync/rsync-exclude.txt",
+                "nily9199@login.rc.colorado.edu:/projects/nily9199/sync/HIA/notebooks/",
+                "/Users/nlyssogor/Documents/research/sync/HIA/notebooks/",
+              },
+            },
+          },
+          ["/Users/nlyssogor/Documents/research/sync/learn-cuda"] = {
+            push = {
+              args = {
+                "-avz",
+                "--delete",
+                "--exclude-from",
+                "/Users/nlyssogor/Documents/research/sync/rsync-exclude.txt",
+                "/Users/nlyssogor/Documents/research/sync/learn-cuda/src/",
+                "nily9199@login.rc.colorado.edu:/projects/nily9199/sync/learn-cuda/src/",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
 }
