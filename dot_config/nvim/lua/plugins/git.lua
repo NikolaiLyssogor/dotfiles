7c08d1bfa250b068edd1559e32ed72ae9ae8a0d6
NikolaiLyssogor <lyssogor@gmail.com>
2025-03-14 21:51:02 -0700

    nvim: keymaps for stage/unstage git hunk

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -5,10 +5,12 @@ return {
     pin = true,
     lazy = false,
     keys = {
-      { "<leader>gd", "<cmd>Gitsigns preview_hunk<CR>", desc = "[d]iff preview" },
-      { "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>",   desc = "[r]eset hunk" },
-      { "<leader>gn", "<cmd>Gitsigns next_hunk<CR>",    desc = "[n]ext hunk" },
-      { "<leader>gp", "<cmd>Gitsigns prev_hunk<CR>",    desc = "[p]revious hunk" },
+      { "<leader>gd", "<cmd>gitsigns preview_hunk<cr>",    desc = "[d]iff preview" },
+      { "<leader>gr", "<cmd>gitsigns reset_hunk<cr>",      desc = "[r]eset hunk" },
+      { "<leader>gn", "<cmd>Gitsigns next_hunk<CR>",       desc = "[n]ext hunk" },
+      { "<leader>gp", "<cmd>Gitsigns prev_hunk<CR>",       desc = "[p]revious hunk" },
+      { "<leader>gs", "<cmd>Gitsigns stage_hunk<CR>",      desc = "[a]dd hunk" },
+      { "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "[u]ndo stage hunk" },
     },
     config = function()
       require("gitsigns").setup()
