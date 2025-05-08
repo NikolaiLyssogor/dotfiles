7624d8cb477398f15a27023957c667ed2b7701ff
NikolaiLyssogor <lyssogor@gmail.com>
2025-05-07 22:48:08 -0700

    nvim: disable sink.nvim dev mode by default

--- dot_config/nvim/lua/plugins/sink.lua
+++ dot_config/nvim/lua/plugins/sink.lua
@@ -1,7 +1,8 @@
 return {
   {
-    dir = "/Users/nlyssogor/Documents/personal-projects/sink.nvim",
-    dev = true,
+    "NikolaiLyssogor/sink.nvim",
+    -- dir = "/Users/nlyssogor/Documents/personal-projects/sink.nvim",
+    -- dev = true,
     keys = {
       { "<leader>rp", "<cmd>SinkPush<CR>", desc = "[p]ush to remote" },
       { "<leader>rl", "<cmd>SinkPull<CR>", desc = "pu[l]l from remote" },
