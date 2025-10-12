39111b4b6724ae2e9e2875a8bbbd88e79eaa6c47
NikolaiLyssogor <lyssogor@gmail.com>
2025-10-11 19:07:38 -0700

    nvim: vim-helm: add plugin

--- dot_config/nvim/lua/plugins/typing-helpers.lua
+++ dot_config/nvim/lua/plugins/typing-helpers.lua
@@ -1,18 +1,23 @@
 return {
-	{
-		"windwp/nvim-autopairs",
-		commit = "3d02855468f94bf435db41b661b58ec4f48a06b7",
-		pin = true,
-		event = "InsertEnter",
-		opts = { map_cr = true }, -- this is equalent to setup({}) function
-	},
-	{
-		"kylechui/nvim-surround",
-		tag = "v2.3.2",
-		pin = true,
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup()
-		end,
-	},
+  {
+    "windwp/nvim-autopairs",
+    commit = "3d02855468f94bf435db41b661b58ec4f48a06b7",
+    pin = true,
+    event = "InsertEnter",
+    opts = { map_cr = true }, -- this is equalent to setup({}) function
+  },
+  {
+    "kylechui/nvim-surround",
+    tag = "v2.3.2",
+    pin = true,
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup()
+    end,
+  },
+  {
+    "towolf/vim-helm",
+    commit = "2c8525fd98e57472769d137317bca83e477858ce",
+    pin = true
+  }
 }
