6a3f52c9650e0f4d77cbaa4b35e1db5f8fcb9cae
NikolaiLyssogor <lyssogor@gmail.com>
2025-10-19 13:26:50 -0700

    update gitsigns

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,23 +1,58 @@
 return {
   {
     "lewis6991/gitsigns.nvim",
-    tag = "v1.0.1",
-
-
-
-
+    commit = "20ad4419564d6e22b189f6738116b38871082332",
     pin = true,
     lazy = false,
-    keys = {
-      { "<leader>gd", "<cmd>Gitsigns preview_hunk_inline<cr>",    desc = "[d]iff preview" },
-      { "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>",      desc = "[r]eset hunk" },
-      { "<leader>gn", "<cmd>Gitsigns next_hunk<CR>",       desc = "[n]ext hunk" },
-      { "<leader>gp", "<cmd>Gitsigns prev_hunk<CR>",       desc = "[p]revious hunk" },
-      { "<leader>gs", "<cmd>Gitsigns stage_hunk<CR>",      desc = "[a]dd hunk" },
-      { "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "[u]ndo stage hunk" },
-    },
     config = function()
-      require("gitsigns").setup()
+      require('gitsigns').setup {
+        on_attach = function(bufnr)
+          local gitsigns = require('gitsigns')
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', '<leader>gn', function()
+            if vim.wo.diff then
+              vim.cmd.normal({ '<leader>gn', bang = true })
+            else
+              gitsigns.nav_hunk('next')
+            end
+          end)
+
+          map('n', '<leader>gp', function()
+            if vim.wo.diff then
+              vim.cmd.normal({ '<leader>gp', bang = true })
+            else
+              gitsigns.nav_hunk('prev')
+            end
+          end)
+
+          -- Actions
+          map('n', '<leader>gs', gitsigns.stage_hunk)
+          map('n', '<leader>gr', gitsigns.reset_hunk)
+
+          map('v', '<leader>gs', function()
+            gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+          end)
+
+          map('v', '<leader>gr', function()
+            gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+          end)
+
+          map('n', '<leader>gd', gitsigns.preview_hunk_inline)
+
+          map('n', '<leader>gb', function()
+            gitsigns.blame_line({ full = true })
+          end)
+
+          map('n', '<leader>gq', function() gitsigns.setqflist('all') end)
+        end
+      }
     end,
   },
 }
