e409a489dddad0085ec7024993d2eb3ff4f1127d
Nikolai Lyssogor <lyssogor@gmail.com>
2026-01-25 17:40:15 -0800

    feat(nvim): <leader>qf to send git changes to qflist

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -11,6 +11,50 @@ return {
       { "<leader>gp", "<cmd>Gitsigns prev_hunk<CR>",       desc = "[p]revious hunk" },
       { "<leader>gs", "<cmd>Gitsigns stage_hunk<CR>",      desc = "[a]dd hunk" },
       { "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "[u]ndo stage hunk" },
+      {
+        "<leader>gq",
+        function()
+          local gitsigns = require("gitsigns")
+
+          local before = vim.fn.getqflist({ id = 0 })
+          local before_id = before.id
+
+          gitsigns.setqflist("all", { open = false })
+
+          local timeout_ms = 1500
+          local interval_ms = 20
+          local start = vim.loop.hrtime()
+
+          ---@return boolean
+          local function timed_out()
+            local elapsed_ms = (vim.loop.hrtime() - start) / 1e6
+            return elapsed_ms >= timeout_ms
+          end
+
+          local function poll()
+            local qf = vim.fn.getqflist({ id = 0, items = 0 })
+
+            if qf.id ~= before_id then
+              if #qf.items == 0 then
+                vim.notify("No hunks.", vim.log.levels.WARN)
+                return
+              end
+              Snacks.picker.qflist()
+              return
+            end
+
+            if timed_out() then
+              vim.notify("Operation to set qickfix list with git hunks timed out.", vim.log.levels.ERROR)
+              return
+            end
+
+            vim.defer_fn(poll, interval_ms)
+          end
+
+          poll()
+        end,
+        desc = "[q]uickfix list hunks"
+      },
     },
   },
 }
