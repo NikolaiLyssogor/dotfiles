b906530c9dddd819a6b18fec5779ebeeb1227190
NikolaiLyssogor <lyssogor@gmail.com>
2026-01-27 16:10:49 -0800

    refactor(nvim): g[ / g] for git hunk navigation

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -7,8 +7,8 @@ return {
     keys = {
       { "<leader>gd", "<cmd>Gitsigns preview_hunk_inline<cr>", desc = "[d]iff preview" },
       { "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>",          desc = "[r]eset hunk" },
-      { "<leader>gn", "<cmd>Gitsigns next_hunk<CR>",           desc = "[n]ext hunk" },
-      { "<leader>gp", "<cmd>Gitsigns prev_hunk<CR>",           desc = "[p]revious hunk" },
+      { "g]",         "<cmd>Gitsigns next_hunk<CR>",           desc = "[n]ext hunk" },
+      { "g[",         "<cmd>Gitsigns prev_hunk<CR>",           desc = "[p]revious hunk" },
       { "<leader>gs", "<cmd>Gitsigns stage_hunk<CR>",          desc = "[a]dd hunk" },
       { "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<CR>",     desc = "[u]ndo stage hunk" },
       {
